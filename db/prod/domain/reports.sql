732d163e5b97f24f2522210a4c1cf309a32b908c
pdorsey <pdorsey@0f581344-5646-3a48-8ded-616bfea576c6>
2008-08-21 22:11:32 +0000

    Moved the REPORTFIELD_REPORTFIELDGROUP comments to above the insert to allow Load Data to work
    git-svn-id: svn://svn.orangeleap.com/clementine/trunk@1845 0f581344-5646-3a48-8ded-616bfea576c6

--- db/prod/domain/reports.sql
+++ db/prod/domain/reports.sql
@@ -50,25 +50,44 @@ insert into REPORTFIELD values(25,b'0',b'0','TRANSACTION_DATE','Date',b'1',b'0',
 insert into REPORTFIELD values(26,b'0',b'1','VALUE','Gift Amount',b'1',b'0',b'0',b'0',b'0',b'0',0);
 
 
-
-insert into REPORTFIELD_REPORTFIELDGROUP values(15,1,1,15); -- Title in People General
-insert into REPORTFIELD_REPORTFIELDGROUP values(1,1,2,1); -- First Name in People General
-insert into REPORTFIELD_REPORTFIELDGROUP values(2,1,3,2); -- Middle Name in People General
-insert into REPORTFIELD_REPORTFIELDGROUP values(3,1,4,3); -- Last Name in People General
-insert into REPORTFIELD_REPORTFIELDGROUP values(14,1,5,14); -- Suffix in People General
-insert into REPORTFIELD_REPORTFIELDGROUP values(10,1,6,10); -- Organization in People General
-insert into REPORTFIELD_REPORTFIELDGROUP values(6,1,7,6); -- E-mail in People General
-insert into REPORTFIELD_REPORTFIELDGROUP values(4,3,8,4); -- Anniversary in People Misc.
-insert into REPORTFIELD_REPORTFIELDGROUP values(5,3,9,5); -- BirthDate in People Misc.
-insert into REPORTFIELD_REPORTFIELDGROUP values(12,3,10,12); -- Spouse First Name in People Misc.
-insert into REPORTFIELD_REPORTFIELDGROUP values(13,3,11,13); -- Spouse Name in People Misc.
-insert into REPORTFIELD_REPORTFIELDGROUP values(17,2,12,17); -- Address Line 1 in People Address
-insert into REPORTFIELD_REPORTFIELDGROUP values(18,2,13,18); -- Address Line 2 in People Address
-insert into REPORTFIELD_REPORTFIELDGROUP values(19,2,14,19); -- Address Line 3 in People Address
-insert into REPORTFIELD_REPORTFIELDGROUP values(20,2,15,20); -- Address Type in People Address
-insert into REPORTFIELD_REPORTFIELDGROUP values(21,2,16,21); -- City in People Address
-insert into REPORTFIELD_REPORTFIELDGROUP values(22,2,17,22); -- Country in People Address
-insert into REPORTFIELD_REPORTFIELDGROUP values(23,2,18,23); -- Postal Code in People Address
-insert into REPORTFIELD_REPORTFIELDGROUP values(25,4,19,25); -- Transaction date into gift general
-insert into REPORTFIELD_REPORTFIELDGROUP values(26,4,19,26); -- Gift Amount into gift general
+-- Title in People General
+insert into REPORTFIELD_REPORTFIELDGROUP values(15,1,1,15); 
+-- First Name in People General
+insert into REPORTFIELD_REPORTFIELDGROUP values(1,1,2,1); 
+-- Middle Name in People General
+insert into REPORTFIELD_REPORTFIELDGROUP values(2,1,3,2); 
+-- Last Name in People General
+insert into REPORTFIELD_REPORTFIELDGROUP values(3,1,4,3); 
+-- Suffix in People General
+insert into REPORTFIELD_REPORTFIELDGROUP values(14,1,5,14); 
+-- Organization in People General
+insert into REPORTFIELD_REPORTFIELDGROUP values(10,1,6,10); 
+-- E-mail in People General
+insert into REPORTFIELD_REPORTFIELDGROUP values(6,1,7,6); 
+-- Anniversary in People Misc.
+insert into REPORTFIELD_REPORTFIELDGROUP values(4,3,8,4); 
+-- BirthDate in People Misc.
+insert into REPORTFIELD_REPORTFIELDGROUP values(5,3,9,5); 
+-- Spouse First Name in People Misc.
+insert into REPORTFIELD_REPORTFIELDGROUP values(12,3,10,12); 
+-- Spouse Name in People Misc.
+insert into REPORTFIELD_REPORTFIELDGROUP values(13,3,11,13); 
+-- Address Line 1 in People Address
+insert into REPORTFIELD_REPORTFIELDGROUP values(17,2,12,17); 
+-- Address Line 2 in People Address
+insert into REPORTFIELD_REPORTFIELDGROUP values(18,2,13,18); 
+-- Address Line 3 in People Address
+insert into REPORTFIELD_REPORTFIELDGROUP values(19,2,14,19); 
+-- Address Type in People Address
+insert into REPORTFIELD_REPORTFIELDGROUP values(20,2,15,20); 
+-- City in People Address
+insert into REPORTFIELD_REPORTFIELDGROUP values(21,2,16,21); 
+-- Country in People Address
+insert into REPORTFIELD_REPORTFIELDGROUP values(22,2,17,22); 
+-- Postal Code in People Address
+insert into REPORTFIELD_REPORTFIELDGROUP values(23,2,18,23); 
+-- Transaction date into gift general
+insert into REPORTFIELD_REPORTFIELDGROUP values(25,4,19,25); 
+-- Gift Amount into gift general
+insert into REPORTFIELD_REPORTFIELDGROUP values(26,4,19,26); 
 
