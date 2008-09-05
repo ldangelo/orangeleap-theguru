58e6c63f18eb752897900e04a429280027ea8259
ldangelo <ldangelo@0f581344-5646-3a48-8ded-616bfea576c6>
2008-09-05 20:02:52 +0000

    Integration with jasperserver
    git-svn-id: svn://svn.orangeleap.com/clementine/trunk@1960 0f581344-5646-3a48-8ded-616bfea576c6

---
+++ src/com/mpower/domain/ReportManager.java
@@ -0,0 +1,24 @@
+package com.mpower.domain;
+
+import java.util.List;
+
+public class ReportManager {
+	List<ReportWizard> reportWizards;
+	
+	
+	public ReportManager() {
+	
+	}
+
+
+	public List<ReportWizard> getReportWizards() {
+		return reportWizards;
+	}
+
+
+	public void setReportWizards(List<ReportWizard> reportWizards) {
+		this.reportWizards = reportWizards;
+	}
+	
+
+}
