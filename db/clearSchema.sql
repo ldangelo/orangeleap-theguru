e722c29ed5556d849e4c618b9ff47085d9fad890
dmeany <dmeany@0f581344-5646-3a48-8ded-616bfea576c6>
2009-04-17 17:49:27 +0000

    git-svn-id: svn://svn.orangeleap.com/clementine/trunk@5496 0f581344-5646-3a48-8ded-616bfea576c6

---
+++ db/clearSchema.sql
@@ -0,0 +1,14 @@
+drop schema IF EXISTS orangeleap;
+create schema orangeleap;
+
+drop schema IF EXISTS theguru;
+create schema theguru;
+
+DROP schema IF EXISTS `company1`;
+create schema company1;
+DROP schema IF EXISTS `company2`;
+create schema company2;
+DROP schema IF EXISTS `company3`;
+create schema company3;
+
+
