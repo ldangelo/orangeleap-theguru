1ce760af6f50475737fd00a28b0da68361bb7071
pdorsey <pdorsey@0f581344-5646-3a48-8ded-616bfea576c6>
2009-04-20 17:46:45 +0000

    CLEMENTINE-246 Installer changes
    git-svn-id: svn://svn.orangeleap.com/clementine/trunk@5526 0f581344-5646-3a48-8ded-616bfea576c6

--- theGuruInstaller/Sql/RunAllSql.bat
+++ theGuruInstaller/Sql/RunAllSql.bat
@@ -1,1 +1,1 @@
-for /f "tokens=*" %%a in ('dir /b %1\Sql\*.sql') do RunSql.bat "%1\MySQL Server 5.0\bin\mySql.exe" theguru orangeleap "%1\Sql\%%a"
+for /f "tokens=*" %%a in ('dir /b %1\Sql\*.sql') do "%1\MySQL Server 5.0\bin\mySql.exe" -u %2 --password=%3 < "%1\Sql\%%a"
--- theGuruInstaller/Sql/RunSql.bat
+++ theGuruInstaller/Sql/RunSql.bat
@@ -1,2 +1,1 @@
-Echo %1 -u %2 --password=%3 < %4 > c:\test.txt
 %1 -u %2 --password=%3 < %4
