e8ef5a1862decd675b5d7b070c6458dad69f86f2
dmeany <dmeany@0f581344-5646-3a48-8ded-616bfea576c6>
2009-10-16 18:57:00 +0000

    can use for report defn temp files
    git-svn-id: svn://svn.orangeleap.com/clementine/trunk@8489 0f581344-5646-3a48-8ded-616bfea576c6

--- src/com/mpower/controller/TempFileUtil.java
+++ src/com/mpower/controller/TempFileUtil.java
@@ -8,22 +8,13 @@ public class TempFileUtil {
 	private static String TEMP_DIR = System.getProperty("theguru.temp.file.dir");
 	private static String JAVA_IO_TEMP_DIR = System.getProperty("java.io.tmpdir");
 	
-	public static File createTempFile(String prefix, String suffix) {
+    // Puts temp files in a separate directory for the instance
+	public static synchronized File createTempFile(String prefix, String suffix) throws IOException {
 		
-		String n = "" + (int)Math.floor(Math.random() * 10000000.0d);
-	    n = n.substring(n.indexOf('.')+1);
-		return new File(TEMP_DIR == null ?  JAVA_IO_TEMP_DIR : TEMP_DIR, prefix + n + suffix); 
+		String dir = TEMP_DIR == null ?  JAVA_IO_TEMP_DIR : TEMP_DIR;
+		return File.createTempFile(prefix, suffix, new File(dir)); 
 		
 	}
 	
-	public static void main(String[] args) {
-		File f = createTempFile("a", ".txt");
-		try {
-			f.createNewFile();
-			System.out.println(f.getAbsolutePath());
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
 	
 }
