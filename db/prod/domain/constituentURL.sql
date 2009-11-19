2aaea557e6e927df74c73d1092ef880bc2784814
ldangelo <ldangelo@0f581344-5646-3a48-8ded-616bfea576c6>
2009-11-19 17:57:55 +0000

    Added support for URL's to The Guru
    git-svn-id: svn://svn.orangeleap.com/clementine/trunk@9075 0f581344-5646-3a48-8ded-616bfea576c6

---
+++ db/prod/domain/constituentURL.sql
@@ -0,0 +1,1 @@
+UPDATE REPORTFIELD set URL="\"http://localhost:8080/orangeleap/constituent.htm?constituentId=\" + $F{CONSTITUENT_CONSTITUENT_ID}",TOOLTIP="\"Show Constiuent in Orane Leap\"" where PRIMARY_KEYS="CONSTITUENT_CONSTITUENT_ID"; 
