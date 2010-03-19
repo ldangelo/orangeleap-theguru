576cf0e3b9275a398f5cb61a2213b5e155844f4d
Erica Green <erica.green@orangeleap.com>
2010-03-19 15:39:22 -0500

    http://community.orangeleap.com/jira/browse/TANGERINE-2094 The GuruSessionData was not getting populated in the api.

--- src/com/mpower/security/ClementineAuthenticationHelper.java
+++ src/com/mpower/security/ClementineAuthenticationHelper.java
@@ -12,6 +12,8 @@ import org.springframework.security.providers.UsernamePasswordAuthenticationToke
 import org.springframework.security.providers.cas.CasAuthenticationToken;
 import org.springframework.security.userdetails.ldap.LdapUserDetails;
 
+import com.mpower.domain.GuruSessionData;
+import com.mpower.util.SessionHelper;
 import com.orangeleap.common.security.OrangeLeapRequestLocal;
 import com.orangeleap.common.security.OrangeLeapUsernamePasswordLocal;
 import com.orangeleap.common.security.OrangeLeapAuthenticationProvider.AuthenticationHelper;
@@ -21,6 +23,8 @@ public class ClementineAuthenticationHelper implements AuthenticationHelper {
 	@Override
 	public void postProcess(Authentication authentication) {
 		Map<String, Object> info = OrangeLeapUsernamePasswordLocal.getOrangeLeapAuthInfo();
+		GuruSessionData sessiondata = SessionHelper.getGuruSessionData();
+		
 		if (authentication instanceof UsernamePasswordAuthenticationToken) {
 
 			UsernamePasswordAuthenticationToken token = (UsernamePasswordAuthenticationToken)authentication;
@@ -30,18 +34,23 @@ public class ClementineAuthenticationHelper implements AuthenticationHelper {
 			}
 			token.setDetails(info);
 			info.put(OrangeLeapUsernamePasswordLocal.PASSWORD, authentication.getCredentials());
+			sessiondata.setPassword(authentication.getCredentials().toString());
 		} else if (authentication instanceof CasAuthenticationToken) {
 			CasAuthenticationToken token = (CasAuthenticationToken) authentication;
 			if (SecurityContextHolder.getContext().getAuthentication() == null)
 				SecurityContextHolder.getContext().setAuthentication(token);
 			token.setDetails(info);
 			info.put(OrangeLeapUsernamePasswordLocal.PASSWORD, ((LdapUserDetails)authentication.getPrincipal()).getPassword());
+			sessiondata.setPassword(((LdapUserDetails)authentication.getPrincipal()).getPassword());
 		}
 		String userName = ((LdapUserDetails)authentication.getPrincipal()).getUsername();
 		String siteName = userName.substring(userName.indexOf('@') +1);
 
+		sessiondata.setUsername(userName);
+		
 		info.put(OrangeLeapUsernamePasswordLocal.USER_NAME,userName);
 		info.put(OrangeLeapUsernamePasswordLocal.SITE,siteName);
+		
 
 		//
 		// let's switch schema's here since we know we are authenticated properly now...
