52dadf38ec68fea8db2f93d7b4b179a14e901db8
alo <alex.lo@orangeleap.com>
2010-05-01 00:22:07 -0500

    changes to mavenize clementine

---
+++ mvn-convert.sh
@@ -0,0 +1,31 @@
+rm -rf src/main
+rm -rf src/test
+mkdir src/main
+mkdir src/test
+mkdir src/main/resources
+mkdir src/main/config
+mkdir src/main/java
+mkdir src/main/webapp
+mkdir src/test/resources
+mkdir src/test/java
+mkdir src/test/config
+mkdir src/main/config/jetty-local
+mkdir db-scripts/src
+mkdir db-scripts/src/main
+mkdir db-scripts/src/main/resources
+cp war/WEB-INF/classes/ws-securityPolicy.xml src/main/resources
+cp theguru.properties src/main/config/jetty-local/
+cp -R src/com src/main/java/com
+cp -R test/com src/test/java/com
+cp -R war/* src/main/webapp
+cp -R config/mpx src/main/config
+#cp src/META-INF/persistence-container.xml src/main/resources/persistence.xml
+sed -e "s/@@DIALECT@@/org.hibernate.dialect.MySQLInnoDBDialect/g" src/META-INF/persistence-container.xml > src/main/resources/persistence.xml
+#cp src/META-INF/persistence-container.xml src/main/config/mpx/persistence.xml 
+sed -e "s/@@DIALECT@@/org.hibernate.dialect.SQLServerDialect/g" src/META-INF/persistence-container.xml > src/main/config/mpx/persistence.xml
+#cp -R config/* src/main/resources
+cp config/jasperreport.dtd src/main/resources
+cp -R db db-scripts/src/main/resources
+cp test/applicationContext.xml src/test/resources
+cp test/testng.xml src/test/resources
+cp test/META-INF/persistence.xml src/test/resources
---
+++ remove-dir.sh
@@ -0,0 +1,12 @@
+rm -rf build/stage
+rm -rf build/tomcat
+rm -rf config
+rm -rf db
+rm -rf lib.old
+rm -rf src/com
+rm -rf src/META-INF
+rm -rf test
+rm -rf theguru.properties
+rm -rf war
+rm -rf src/main/webapp/WEB-INF/classes
+
