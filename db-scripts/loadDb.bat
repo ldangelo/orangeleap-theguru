4d60b95833e169f8cd4cd5f44463c80d32ded26a
alo <alex.lo@orangeleap.com>
2010-05-12 11:18:41 -0500

    Added back "9.1 Load Split Database Data" from ant to db-scripts pom.xml for clementine

---
+++ db-scripts/loadDb.bat
@@ -0,0 +1,4 @@
+mvn initialize
+mvn initialize -DtheGuruSchema=company1theguru -DolSchema=company1 -DschemaSql=company1
+mvn initialize -DtheGuruSchema=demotheguru -DolSchema=demo -DschemaSql=company1
+mvn initialize -DtheGuruSchema=sandboxtheguru -DolSchema=sandbox -DschemaSql=company1
