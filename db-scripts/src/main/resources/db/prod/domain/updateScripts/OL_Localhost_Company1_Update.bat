07ee0544799fc3480e6becdecf70f4f3fb6e5a42
Erica Green <erica.green@orangeleap.com>
2010-06-10 15:55:24 -0500

    http://community.orangeleap.com/jira/browse/CLEMENTINE-864 Date Custom Fields were not showing up in the guru.

--- db-scripts/src/main/resources/db/prod/domain/updateScripts/OL_Localhost_Company1_Update.bat
+++ db-scripts/src/main/resources/db/prod/domain/updateScripts/OL_Localhost_Company1_Update.bat
@@ -1,5 +1,5 @@
 cls
 
-java -jar C:\dev\apps\eclipse-3.4\workspace-trunk\DatabaseUpdateUtility\deploy\databaseupdateutility.jar -databasename company1theguru -username orangeleap -password orangeleap -port 3306 -servername localhost -sqlscriptdirectory "C:\dev\apps\eclipse-3.4\workspace-trunk\Clementine\db\prod\domain\updateScripts\orangeLeap" -updatetype s
+java -jar C:\dev\apps\eclipse-3.5.1\workspace\DatabaseUpdateUtility\deploy\databaseupdateutility.jar -databasename company1theguru -username orangeleap -password orangeleap -port 3306 -servername localhost -sqlscriptdirectory "C:\dev\apps\eclipse-3.5.1\workspace\clementine\db-scripts\src\main\resources\db\prod\domain\updateScripts\orangeLeap" -updatetype s
 
 pause
