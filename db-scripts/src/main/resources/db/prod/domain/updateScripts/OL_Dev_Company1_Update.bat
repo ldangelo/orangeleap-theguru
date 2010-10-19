eb8430956a62c12d6924864a9ed27a83812713d0
Erica Green <erica.green@orangeleap.com>
2010-10-19 15:03:05 -0500

    http://community.orangeleap.com/jira/browse/CLEMENTINE-967 working on charting

--- db-scripts/src/main/resources/db/prod/domain/updateScripts/OL_Dev_Company1_Update.bat
+++ db-scripts/src/main/resources/db/prod/domain/updateScripts/OL_Dev_Company1_Update.bat
@@ -1,5 +1,5 @@
 cls
 
-java -jar C:\dev\apps\eclipse-3.4\workspace-trunk\DatabaseUpdateUtility\deploy\databaseupdateutility.jar -databasename company1theguru -username orangeleap -password orangeleap -port 10006 -servername 10.0.2.62 -sqlscriptdirectory "C:\dev\apps\eclipse-3.4\workspace-trunk\Clementine\db\prod\domain\updateScripts\orangeLeap" -updatetype s
+java -jar C:\dev\apps\eclipse-3.5.1\workspace\DatabaseUpdateUtility\deploy\databaseupdateutility.jar -databasename company1theguru -username orangeleap -password orangeleap -port 10006 -servername 10.0.2.62 -sqlscriptdirectory "C:\dev\apps\eclipse-3.5.1\workspace\clementine\db-scripts\src\main\resources\db\prod\domain\updateScripts\OrangeLeap" -updatetype s
 
 pause
